a25f08968910b06e80687d7de3652fc08660e8a4
Emily <emilyadarr@gmail.com>
2021-12-20 16:33:13 -0500

    first question

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,85 @@ var questionEl = document.getElementById("question");
 var optionsEl = document.getElementById("options");
 var answerEl = document.getElementById("answer");
 var startBtn = document.getElementById("start-btn");
+var timeLeft = 70;
 
-var question1 = "Commonly used data types DO NOT include:"
+
+var option1 = document.createElement("button");
+var option1Answers = ["strings", "quotes"]
+var option2 = document.createElement("button");
+var option2Answers = ["booleans", "curly brackets"]
+var option3 = document.createElement("button");
+var option3Answers = ["alerts", "parenthesis"]
+var option4 = document.createElement("button");
+var option4Answers = ["numbers", "square brackets"]
+
+var questionElQuestions = ["Commonly used data types DO NOT include:", "The condition in an if/else statement is enclosed with _______."];
+
+// var questionObj = {
+//   question: ["Commonly used data types DO NOT include:", "The condition in an if/else statement is enclosed with _______."],
+//   option1: ["strings", "quotes"],
+//   option2: ["booleans", "curly brackets"],
+//   option3: ["alerts", "parenthesis"],
+//   option4: ["numbers", "square brackets"]
+// };
+
+option1.className = "btn";
+option2.className = "btn";
+option3.className = "btn";
+option4.className = "btn";
 
 function startQuiz() {
   timer();
+  question1();
 };
 
+function question1() {
+  // for (var i = 0; i < questionObj.length; i++) {
+  //   questionEl.textContent = questionObj.question[i];
+  //   optionsEl.textContent = "";
+  //   option1.textContent = "1. " + questionObj.option1[i];
+  //   option2.textContent = "2. " + questionObj.option2[i];
+  //   option3.textContent = "3. " + questionObj.option3[i];
+  //   option4.textContent = "4. " + questionObj.option4[i];
+  //   optionsEl.appendChild(option1);
+  //   optionsEl.appendChild(option2);
+  //   optionsEl.appendChild(option3);
+  //   optionsEl.appendChild(option4);
+  // }
+
+  for (var i = 0; i < questionElQuestions.length; i++) {
+    questionEl.textContent = questionElQuestions[i];
+    optionsEl.textContent = "";
+    option1.textContent = "1. " + option1Answers[i];
+    option2.textContent = "2. " + option2Answers[i];
+    option3.textContent = "3. " + option3Answers[i];
+    option4.textContent = "4. " + option4Answers[i];
+    optionsEl.appendChild(option1);
+    optionsEl.appendChild(option2);
+    optionsEl.appendChild(option3);
+    optionsEl.appendChild(option4);
+
+    if (questionElQuestions[0]) {
+      option1.addEventListener("click", wrong);
+      option2.addEventListener("click", wrong);
+      option3.addEventListener("click", correct);
+      option4.addEventListener("click", wrong);
+    }
+  }
+};
+
+function correct() {
+  answerEl.textContent = "Correct!";
+  question1();
+}
+
+function wrong() {
+  answerEl.textContent = "Wrong!";
+  timeLeft = timeLeft -10;
+  question1();
+}
+
 function timer() {
-  var timeLeft = 70;
 
   var timeInterval = setInterval(function() {
     if (timeLeft < 0) {
