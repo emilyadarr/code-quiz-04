77ff7b1c269db98e1a82a7f6e7599734523b5ceb
Emily <emilyadarr@gmail.com>
2021-12-22 12:43:15 -0500

    added questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,23 +7,39 @@ var timeLeft = 70;
 
 
 var option1 = document.createElement("button");
-var option1Answers = ["strings", "quotes"]
 var option2 = document.createElement("button");
-var option2Answers = ["booleans", "curly brackets"]
 var option3 = document.createElement("button");
-var option3Answers = ["alerts", "parenthesis"]
 var option4 = document.createElement("button");
-var option4Answers = ["numbers", "square brackets"]
 
-var questionElQuestions = ["Commonly used data types DO NOT include:", "The condition in an if/else statement is enclosed with _______."];
+var currentQuestionIndex = 0;
 
-// var questionObj = {
-//   question: ["Commonly used data types DO NOT include:", "The condition in an if/else statement is enclosed with _______."],
-//   option1: ["strings", "quotes"],
-//   option2: ["booleans", "curly brackets"],
-//   option3: ["alerts", "parenthesis"],
-//   option4: ["numbers", "square brackets"]
-// };
+var questions = [
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "The condition in an if/else statement is enclosed with _______.",
+    choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+    answer: "parenthesis"
+  },
+  {
+    title: "Arrays in JavaScript can be used to store ______.",
+    choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    answer: ""
+  },
+  {
+    title: "String values must be enclosed within _____ when being assigned to variables.",
+    choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+    answer: "quotes"
+  },
+  {
+    title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+    answer: ""
+  }
+];
 
 option1.className = "btn";
 option2.className = "btn";
@@ -35,50 +51,49 @@ function startQuiz() {
   question1();
 };
 
-function question1() {
+//questionObj.forEach(questionFunc);
+//function questionFunc() {
   // for (var i = 0; i < questionObj.length; i++) {
-  //   questionEl.textContent = questionObj.question[i];
-  //   optionsEl.textContent = "";
-  //   option1.textContent = "1. " + questionObj.option1[i];
-  //   option2.textContent = "2. " + questionObj.option2[i];
-  //   option3.textContent = "3. " + questionObj.option3[i];
-  //   option4.textContent = "4. " + questionObj.option4[i];
-  //   optionsEl.appendChild(option1);
-  //   optionsEl.appendChild(option2);
-  //   optionsEl.appendChild(option3);
-  //   optionsEl.appendChild(option4);
-  // }
 
-  for (var i = 0; i < questionElQuestions.length; i++) {
-    questionEl.textContent = questionElQuestions[i];
-    optionsEl.textContent = "";
-    option1.textContent = "1. " + option1Answers[i];
-    option2.textContent = "2. " + option2Answers[i];
-    option3.textContent = "3. " + option3Answers[i];
-    option4.textContent = "4. " + option4Answers[i];
+
+function question1() {
+    var currentQuestion = questions[currentQuestionIndex];
+    console.log(currentQuestionIndex);
+    questionEl.textContent = currentQuestion.title;
+  
+    optionsEl.innerHTML ="";
+    option1.textContent = "1. " + currentQuestion.choices[0];
+    option2.textContent = "2. " + currentQuestion.choices[1];
+    option3.textContent = "3. " + currentQuestion.choices[2];
+    option4.textContent = "4. " + currentQuestion.choices[3];
     optionsEl.appendChild(option1);
     optionsEl.appendChild(option2);
     optionsEl.appendChild(option3);
     optionsEl.appendChild(option4);
-
-    if (questionElQuestions[0]) {
+  
+    if (currentQuestionIndex===0) {
       option1.addEventListener("click", wrong);
       option2.addEventListener("click", wrong);
       option3.addEventListener("click", correct);
       option4.addEventListener("click", wrong);
     }
   }
-};
 
 function correct() {
   answerEl.textContent = "Correct!";
-  question1();
+  if (currentQuestionIndex < questions.length) {
+    currentQuestionIndex++;
+    question1();
+  }
 }
 
 function wrong() {
   answerEl.textContent = "Wrong!";
   timeLeft = timeLeft -10;
-  question1();
+  if (currentQuestionIndex < questions.length) {
+    currentQuestionIndex++;
+    question1();
+  }
 }
 
 function timer() {
